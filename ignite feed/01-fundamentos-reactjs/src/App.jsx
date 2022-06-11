d9a3b57000bf3211b9824b16189554dd78adb9b4
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2022-06-11 01:32:58 -0400

    Criando a sidebar

--- ignite feed/01-fundamentos-reactjs/src/App.jsx
+++ ignite feed/01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,31 @@
-import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
 
 export function App() {
   return (
     <div>
       <Header />
 
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Marcos Vini"
+            content="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          />
+          <Post 
+            author="bruno"
+            content="aaaaasdasdassssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          />
+        </main>
+      </div>
     </div>
   )
 }
--- ignite feed/READMI.md
+++ ignite feed/READMI.md
@@ -1,2 +1,7 @@
 # Para criar a aplicação com o Vite 
 -- npm create vite@latest
+
+# biblioteca de icones
+-- phosphoricons
+
+--> npm i phosphor-react
